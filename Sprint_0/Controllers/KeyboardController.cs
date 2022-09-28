640e0de5382ea76edd9fbefaa08bcd35c8879998
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2022-09-28 10:52:15 -0400

    vommit

--- Sprint_0/Controllers/KeyboardController.cs
+++ Sprint_0/Controllers/KeyboardController.cs
@@ -75,6 +75,8 @@ public class KeyboardController : IController
 		foreach(Keys key in pressedKeys)
 		{
 			controllerMappings[key].Execute();
+			//Add functionality for when it do not work
+				controllerMappings[key].Execute();
 		}
 	}
 
