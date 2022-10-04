50fa53a0ee3b690f32c40770fed9986c400f1610
hom-john <homjohn00@gmail.com>
2022-10-03 23:48:24 -0400

    Minor changes like comments and names

---
+++ Sprint_0/States/BlockStates/StairsState.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+public class StairsState : ITileState
+{
+    private Tile tile;
+    private static List<Rectangle> itemSprites = new List<Rectangle>()
+    {
+        new Rectangle(1, 11, 16, 16),
+        new Rectangle(19, 11, 16, 16)
+    };
+    private int currentIndex;
+
+    public StairsState(Tile tile)
+    {
+        this.tile = tile;
+        currentIndex = 0;
+    }
+
+    public void Next()
+    {
+        tile.state = new Statue1State(tile);
+    }
+    public void Prev()
+    {
+        tile.state = new BlueGapState(tile);
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        Texture2D tile1 = Texture2DStorage.GetTile8Sprite();
+        Rectangle sourceRect = itemSprites[currentIndex];
+        tile.DrawSprite(spriteBatch, tile1, sourceRect);
+
+    }
+    public void Update()
+    {
+        // call something like goomba.MoveLeft() or goomba.Move(-x,0);
+    }
+}
---
+++ Sprint_0/States/BlockStates/WhiteBrickState.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+public class WhiteBrickState : ITileState
+{
+    private Tile tile;
+    private static List<Rectangle> itemSprites = new List<Rectangle>()
+    {
+        new Rectangle(1, 11, 16, 16),
+        new Rectangle(19, 11, 16, 16)
+    };
+    private int currentIndex;
+    public WhiteBrickState(Tile tile)
+    {
+        this.tile = tile;
+        currentIndex = 0;
+    }
+
+    public void Next()
+    {
+        tile.state = new BlackTileState(tile);
+    }
+    public void Prev()
+    {
+        tile.state = new PushBlockState(tile);
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        Texture2D tile1 = Texture2DStorage.GetTile2Sprite();
+        Rectangle sourceRect = itemSprites[currentIndex];
+        tile.DrawSprite(spriteBatch, tile1, sourceRect);
+
+    }
+    public void Update()
+    {
+        // call something like goomba.MoveLeft() or goomba.Move(-x,0);
+    }
+}
