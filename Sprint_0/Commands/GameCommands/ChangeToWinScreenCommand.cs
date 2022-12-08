2763625172e29ce86ba16f445467cc96689b3b79
dflippojr <dflippo.jr@gmail.com>
2022-12-07 21:59:09 -0500

    changed content files, fixed keyboard controller

--- Sprint_0/Commands/GameCommands/ChangeToWinScreenCommand.cs
+++ Sprint_0/Commands/GameCommands/ChangeToWinScreenCommand.cs
@@ -2,12 +2,12 @@ using Microsoft.Xna.Framework.Graphics;
 using Sprint_0;
 using System;
 
-public class ChangeToDeathScreenCommand : ICommand
+public class ChangeToWinScreenCommand : ICommand
 {
 
     private Game1 game;
 
-    public ChangeToDeathScreenCommand(Game1 game)
+    public ChangeToWinScreenCommand(Game1 game)
     {
         this.game = game;
     }
--- Sprint_0/Controllers/KeyboardController.cs
+++ Sprint_0/Controllers/KeyboardController.cs
@@ -90,6 +90,22 @@ public class KeyboardController : IController
                 }
                 buildCommands.state = pressedKeys; //sets state to compare to new pressed keys
                 break;
+
+            //Win Screen
+            case "Sprint_0.GameStates.WinScreenState":
+                foreach (Keys key in pressedKeys)
+                {
+                    //checks for registered commands
+                    if (buildCommands.startupControllerMappings.ContainsKey(key))
+                    {
+                        if (buildCommands.state.Contains(key)) //checks for the commands in state
+                        {
+                            buildCommands.startupControllerMappings[key].Execute();
+                        }
+                    }
+                }
+                buildCommands.state = pressedKeys; //sets state to compare to new pressed keys
+                break;
         }
 	}
 }
